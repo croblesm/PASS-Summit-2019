48de8498877ea9dea52b971b7b6757b55f99bf10
Carlos Robles <dbamastery@outlook.com>
2019-11-06 14:37:38 -0700

    Add files via upload

---
+++ Demo_01/1_5_DownloadBackupAzure.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#==============================================================================
+# Script name   : 1_5_DownloadBackupAzure.sh
+# Description   : Script to download backup file from Azure
+# Author        : Carlos Robles
+# Email         : crobles@dbamastery.com
+# Twitter       : @dbamastery
+# Date          : 20191106
+# 
+# Version       : 3.0   
+# Usage         : bash 1_5_DownloadBackupAzure.sh
+#
+# Notes         : This script assumes AZ CLI is installed and configured
+#==============================================================================
+
+# Setting storage account settings
+export  AZURE_STORAGE_ACCOUNT="dbamastery"
+export  AZURE_STORAGE_KEY="6aVnL92Y0ha4IRG2OB/vhKAuJynMia68t9ORB1/U99Trzbxr9E4xiO4/UBkvchK7Z2CpM4Dd3iR6K7YmPn4oew=="
+container_name="sqlbackupfiles"
+backup_dir=/Users/carlos/Documents/Summit_2019/Backups
+
+echo -e "Starting script to download backups from Azure to local machine\n ..."
+
+# Getting blob container name
+echo -e "\nBlob container name:\t $container_name"
+
+echo -e "\nListing blobs in $container_name container...\n"
+az storage blob list --container-name $container_name --query '[].{BlobName:name, CreationDate:properties.creationTime}' -o table
+
+# Getting last backup file
+#export blob_name=`az storage blob list --container-name $container_name --output table | awk 'NR == 3 {print $1}'`
+#last_backup=`az storage blob list --container-name $container_name --output table | sort -r |awk 'NR == 1 {print $1}'`
+last_backup=`az storage blob list --container-name $container_name --output table |awk 'NR == 3 {print $1}'`
+echo -e "\nBackup file to download:\t $last_backup"
+
+# Downloading last backup file
+echo -e "\nDownloading bak file ...\n"
+az storage blob download --container-name $container_name --name $last_backup --file $backup_dir/$last_backup --output table
+
+echo -e "\nBackup downloaded succesfully:\n"
+ls /Users/carlos/Documents/Summit_2019/Backups/*.bak
+
+echo -e "\n\nEnd of script"
